ba10a62afeda3dfbf0614d1876b42b39601ce85f
lucaxsebastian <lucassebastian167@gmail.com>
2025-04-17 17:01:00 -0300

    feat: add controller

---
+++ VerticalSliceTodoList/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using vertical_slice_with_aspnet.VerticalSliceTodoList.Features_TodoItems.Commands;
+
+namespace vertical_slice_with_aspnet.VerticalSliceTodoList.Controllers;
+
+[ApiController]
+[Route("todo")]
+[Produces("application/json")]
+public class TodoController(IMediator mediator) : ControllerBase
+{
+    private readonly IMediator _mediator = mediator;
+
+    [HttpPost]
+    public async Task<IActionResult> AddTodoItem([FromBody] AddTodoItemCommand command, CancellationToken cancellation = default)
+    {
+        var result = await _mediator.Send(command, cancellation);
+
+        //return result.ToActionResult(
+        //    onSucess: result =>
+        //    {
+        //        return new CreatedAtActionResult(
+        //            nameof(GetTodoItem), "todo", new { id = result.Value }, result.Value);
+        //    });
+    }
+
+}
